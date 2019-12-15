489f9255594b2a0c4784676362f9bfd99d70b1da
jefery_only <darkarchana@gmail.com>
2019-12-15 21:14:01 +0700

    Database Change and Adding Simple Link Encoding

--- service/serviceimpl/chapter_serviceimpl.go
+++ service/serviceimpl/chapter_serviceimpl.go
@@ -1,8 +1,12 @@
 package service
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 	"log"
+	"regexp"
+	"strconv"
 
 	dao "github.com/darkarchana/darkarchana-backend/dao/daoimpl"
 	"github.com/darkarchana/darkarchana-backend/model"
@@ -75,9 +79,45 @@ func modelToView(model model.Chapter) view.Chapter {
 	view := view.Chapter{
 		Chapter: model.Chapter,
 		Page:    model.Page,
-		Link:    model.Link,
+		Link:    encodeLink(model.Link),
 	}
 	return view
+}
+
+func encodeLink(s string) string {
+	binaryString := toBinaryRunes(s)
+	regex, _ := regexp.Compile("[0|1]{8}")
+	match := regex.FindAllString(binaryString, -1)
+	byteString := make([]byte, 0)
+
+	for _, v := range match {
+		intVal, _ := strconv.ParseUint(v, 2, 8)
+		codedIntValue := (intVal + 2) % 256
+		byteString = append(byteString, byte(codedIntValue))
+	}
+	return string(byteString)
+}
+
+func decodeLink(s string) string {
+	binaryString := toBinaryRunes(s)
+	regex, _ := regexp.Compile("[0|1]{8}")
+	match := regex.FindAllString(binaryString, -1)
+	byteString := make([]byte, 0)
+
+	for _, v := range match {
+		intVal, _ := strconv.ParseUint(v, 2, 8)
+		decodedIntValue := (intVal + 256 - 2) % 256
+		byteString = append(byteString, byte(decodedIntValue))
+	}
+	return string(byteString)
+}
+
+func toBinaryRunes(s string) string {
+	var buffer bytes.Buffer
+	for _, runeValue := range s {
+		fmt.Fprintf(&buffer, "%08b", runeValue)
+	}
+	return fmt.Sprintf("%s", buffer.Bytes())
 }
 
 // ChapterServiceImpl : Implementation of Interface ChapterService
